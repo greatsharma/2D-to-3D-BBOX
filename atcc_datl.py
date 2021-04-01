6f24b74cdeeae17aa34aef2860ee532bbc94f1a2
greatsharma <gsharma81999@gmail.com>
2021-04-01 16:13:13 +0530

    atcc_datl ready with realtime merged points

--- atcc_datl.py
+++ atcc_datl.py
@@ -1,5 +1,12 @@
+import os
 import cv2
+import time
+import datetime
+import subprocess
 import numpy as np
+from utils import init_lane_detector
+from camera_metadata import CAMERA_METADATA
+from detectors.trt_detector import TrtYoloDetector
 
 
 vidcap1 = cv2.VideoCapture("inputs/datl_clip1.mp4")
@@ -10,9 +17,41 @@ vidcap2 = cv2.VideoCapture("inputs/datl_clip2.mp4")
 width2 = int(vidcap2.get(cv2.CAP_PROP_FRAME_WIDTH))
 height2 = int(vidcap2.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-vidcap2.set(cv2.CAP_PROP_POS_FRAMES, 500)
+vidcap2.set(cv2.CAP_PROP_POS_FRAMES, 505)
+
+_, initial_frame1 = vidcap1.read()
+_, initial_frame2 = vidcap2.read()
+
+initial_frame1 = cv2.resize(initial_frame1, dsize=(width1//2, height1//2))
+
+camera_meta = CAMERA_METADATA["datl"]
+
+detector = TrtYoloDetector(
+    initial_frame1,
+    init_lane_detector(camera_meta),
+    detection_thresh=0.5,
+)
+
+date = datetime.datetime.now()
+date = date.strftime("%d_%m_%Y_%H:%M:%S")
+date = date.replace(":", "")
+
+curr_folder = "outputs/datl/" + date
+
+if not os.path.exists(curr_folder):
+    os.mkdir(curr_folder)
+
+outvideo_path = curr_folder + "/linear_mapping.avi"
+
+videowriter = cv2.VideoWriter(
+    outvideo_path,
+    cv2.VideoWriter_fourcc("M", "J", "P", "G"),
+    30.0,
+    (1920, 540),
+)
 
 while vidcap1.isOpened() and vidcap2.isOpened():
+    start_time = time.time()
 
     status1, frame1 = vidcap1.read()
     status2, frame2 = vidcap2.read()
@@ -25,10 +64,32 @@ while vidcap1.isOpened() and vidcap2.isOpened():
         print("status2 false")
         break
 
-    frame1 = cv2.resize(frame1, dsize=(width1//2, int(height1/2.5)))
-    frame2 = cv2.resize(frame2, dsize=(width2//2, int(height2/2.5)))
+    frame1 = cv2.resize(frame1, dsize=(width1//2, height1//2))
+    frame2 = cv2.resize(frame2, dsize=(width2//2, height2//2))
 
-    final_frame = np.vstack((frame1, frame2))
+    # for l in ["leftlane", "rightlane"]:
+    #     cv2.polylines(frame1, [camera_meta[f"{l}_coords"]], isClosed=True, color=(0, 0, 0), thickness=2)
+    #     cv2.circle(frame1, camera_meta[f"{l}_ref"], radius=4, color=(0, 0, 255), thickness=-1)
+
+    detection_list, axles = detector.detect(frame1)
+
+    for det in detection_list:
+        rect = det["rect"]
+        btm = det["obj_bottom"]
+        cv2.rectangle(frame1, rect[:2], rect[2:], (0,225,0), 2)
+        cv2.circle(frame1, btm, 3, (0,0,255), -1)
+
+        btmx_tf = (0.44958882 * btm[0]) + (-3.23493889 * btm[1]) + 1299.903505513684
+        btmy_tf = (0.41243032 * btm[0]) + (-0.07925315 * btm[1]) + 116.09248088227169
+
+        # btmx_tf = (0.39795221 * btm[0]) + (-3.06747396 * btm[1]) + 1269.8043491220583
+        # btmy_tf = (0.38150148 * btm[0]) + (-0.01415993 * btm[1]) + 109.26557888289531
+
+        cv2.circle(frame2, (int(btmx_tf), int(btmy_tf)), 3, (0,0,255), -1)
+
+    final_frame = np.hstack((frame1, frame2))
+    videowriter.write(final_frame)
+
     cv2.imshow("video", final_frame)
 
     key = cv2.waitKey(1)
@@ -37,7 +98,27 @@ while vidcap1.isOpened() and vidcap2.isOpened():
        break
     elif key == ord('p'):
         cv2.waitKey(-1)
+    
+    print(f"fps: {1.0 / (time.time()-start_time)}")
 
 vidcap1.release()
 vidcap2.release()
 cv2.destroyAllWindows()
+
+status = subprocess.call(
+    [
+        "ffmpeg",
+        "-i",
+        outvideo_path,
+        "-vcodec",
+        "libx264",
+        "-crf",
+        "30",
+        curr_folder + "/linear_mapping_comp.avi",
+    ]
+)
+
+if status:
+    print(f"\n unable to compress {outvideo_path} ! \n")
+else:
+    os.remove(outvideo_path)
--- utils/visualize_frame.py
+++ utils/visualize_frame.py
@@ -1,36 +1,82 @@
 import cv2
-import math
+import numpy as np
 import matplotlib.pyplot as plt
 
 
-# input_path = "inputs/kurnul_clip2.avi"
-input_path = "inputs/hyderabad_clip2.avi"
+vidcap1 = cv2.VideoCapture("inputs/datl_clip1.mp4")
+width1 = int(vidcap1.get(cv2.CAP_PROP_FRAME_WIDTH))
+height1 = int(vidcap1.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-vidcap = cv2.VideoCapture(input_path)
+vidcap2 = cv2.VideoCapture("inputs/datl_clip2.mp4")
+width2 = int(vidcap2.get(cv2.CAP_PROP_FRAME_WIDTH))
+height2 = int(vidcap2.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-while vidcap.isOpened():
-    status, frame = vidcap.read()
+vidcap2.set(cv2.CAP_PROP_POS_FRAMES, 504)
+frame_count = 0
 
-    frame = cv2.resize(frame, dsize=(960, 540), interpolation=cv2.INTER_LINEAR)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+coords = [
+    [(586, 369), (89, 250)],
+    [(874, 377), (131, 366)],
+    [(741, 333), (281, 312)],
+    [(385, 289), (313, 196)],
+    [(305, 255), (405, 173)],
+    [(526, 265), (489, 240)],
+    [(443, 236), (565, 212)],
+    [(192, 209), (535, 143)],
+    [(314, 194), (674, 176)],
+    [(261, 177), (716, 160)],
+    [(78, 162), (668, 113)],
+    [(48, 148), (702, 104)],
+    [(180, 150), (780, 139)],
+    [(143, 138), (807, 128)],
+    [(90, 121), (847, 115)],
+    [(65, 113), (866, 108)],
+    [(137, 208), (504, 129)],
+    [(25, 203), (466, 103)],
+    [(306, 364), (44, 176)]
+]
 
-    pt1, pt2 = (900, 372), (200, 193)
-    # pt1, pt2 = (866,384), (196, 186)
-    dy, dx = (pt2[1] - pt1[1]), (pt2[0] - pt1[0])
-    angle1 = math.atan2(dy, dx)
-    print(angle1) # -166.68 (deg), -2.91 (rad)
-    cv2.arrowedLine(frame, pt1, pt2, color=(0,0,255), thickness=3)
+while vidcap1.isOpened() and vidcap2.isOpened():
 
-    pt1, pt2 = (756, 477), (93, 225)
-    # pt1, pt2 = (717, 483), (100, 210)
-    dy, dx = (pt2[1] - pt1[1]), (pt2[0] - pt1[0])
-    angle1 = math.atan2(dy, dx)
-    print(angle1) # -160.71 (deg), -2.80 (rad)
-    cv2.arrowedLine(frame, pt1, pt2, color=(255,0,0), thickness=3)
+    status1, frame1 = vidcap1.read()
+    status2, frame2 = vidcap2.read()
 
-    plt.imshow(frame)
+    if not status1:
+        print("status1 false")
+        break
+
+    if not status2:
+        print("status2 false")
+        break
+
+    frame_count += 1
+
+    frame1 = cv2.resize(frame1, dsize=(width1//2, height1//2))
+    frame2 = cv2.resize(frame2, dsize=(width2//2, height2//2))
+
+    for c in coords:
+        cv2.circle(frame1, (c[0][0], int(c[0][1] / 432 * 540)), radius=1, color=(0, 0, 255), thickness=-1)
+        cv2.circle(frame2, (c[1][0], int(c[1][1] / 432 * 540)), radius=1, color=(0, 0, 255), thickness=-1)
+
+    frame1 = cv2.cvtColor(frame1, code=cv2.COLOR_BGR2RGB)
+
+    plt.imshow(frame1)
     plt.show()
     break
 
-vidcap.release()
+    # final_frame = np.vstack((frame1, frame2))
+    # cv2.imshow("video", final_frame)
+
+    # if frame_count == 550:
+    #     cv2.waitKey(-1)
+
+    # key = cv2.waitKey(1)
+
+    # if key == ord('q'):
+    #    break
+    # elif key == ord('p'):
+    #     cv2.waitKey(-1)
+
+vidcap1.release()
+vidcap2.release()
 cv2.destroyAllWindows()
