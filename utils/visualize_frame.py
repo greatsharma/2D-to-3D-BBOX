f51634487a613e88317810295199f70eb89cef48
greatsharma <gsharma81999@gmail.com>
2021-04-01 19:42:35 +0530

    updated visualize_frame.py

--- utils/visualize_frame.py
+++ utils/visualize_frame.py
@@ -3,11 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-vidcap1 = cv2.VideoCapture("inputs/datl_clip1.mp4")
+vidcap1 = cv2.VideoCapture("inputs/datlcam1_clip1.mp4")
 width1 = int(vidcap1.get(cv2.CAP_PROP_FRAME_WIDTH))
 height1 = int(vidcap1.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-vidcap2 = cv2.VideoCapture("inputs/datl_clip2.mp4")
+vidcap2 = cv2.VideoCapture("inputs/datlcam2_clip1.mp4")
 width2 = int(vidcap2.get(cv2.CAP_PROP_FRAME_WIDTH))
 height2 = int(vidcap2.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
@@ -54,13 +54,35 @@ while vidcap1.isOpened() and vidcap2.isOpened():
     frame1 = cv2.resize(frame1, dsize=(width1//2, height1//2))
     frame2 = cv2.resize(frame2, dsize=(width2//2, height2//2))
 
+    llcoords = np.array(
+                [(200, 520), (850, 520), (105, 180), (10, 228), (10, 330)], dtype=np.int32
+            ).reshape((-1, 1, 2))
+    rlcoords = np.array(
+                [(850, 520), (950, 316), (223, 110), (105, 180)], dtype=np.int32
+            ).reshape((-1, 1, 2))
+
+    cv2.polylines(frame1, [llcoords], isClosed=True, color=(0, 0, 0), thickness=2)
+    cv2.polylines(frame1, [rlcoords], isClosed=True, color=(0, 0, 0), thickness=2)
+    cv2.circle(frame1, (878, 533), radius=4, color=(0, 0, 255), thickness=-1)
+    cv2.circle(frame1, (924, 467), radius=4, color=(0, 0, 255), thickness=-1)
+
+    llcoords = np.array(
+                [(20, 185), (20, 412), (743, 146), (505, 91)], dtype=np.int32
+            ).reshape((-1, 1, 2))
+    rlcoords = np.array(
+                [(20, 412), (20, 525), (462, 525), (934, 187), (743, 146)], dtype=np.int32
+            ).reshape((-1, 1, 2))
+
+    cv2.polylines(frame2, [llcoords], isClosed=True, color=(0, 0, 0), thickness=2)
+    cv2.polylines(frame2, [rlcoords], isClosed=True, color=(0, 0, 0), thickness=2)
+    cv2.circle(frame2, (5, 300), radius=4, color=(0, 0, 255), thickness=-1)
+    cv2.circle(frame2, (5, 535), radius=4, color=(0, 0, 255), thickness=-1)
+
     for c in coords:
         cv2.circle(frame1, (c[0][0], int(c[0][1] / 432 * 540)), radius=1, color=(0, 0, 255), thickness=-1)
         cv2.circle(frame2, (c[1][0], int(c[1][1] / 432 * 540)), radius=1, color=(0, 0, 255), thickness=-1)
 
-    frame1 = cv2.cvtColor(frame1, code=cv2.COLOR_BGR2RGB)
-
-    plt.imshow(frame1)
+    plt.imshow(cv2.cvtColor(frame2, code=cv2.COLOR_BGR2RGB))
     plt.show()
     break
 
