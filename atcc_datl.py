69cb2534dd4cd1ffc3882431864c348ae313123a
greatsharma <gsharma81999@gmail.com>
2021-04-09 12:47:33 +0530

    corrected linear regression coefficients for mapping

--- atcc_datl.py
+++ atcc_datl.py
@@ -4,6 +4,7 @@ import time
 import datetime
 import subprocess
 import numpy as np
+
 from utils import init_lane_detector
 from camera_metadata import CAMERA_METADATA
 from detectors.trt_detector import TrtYoloDetector
@@ -29,7 +30,7 @@ camera_meta = CAMERA_METADATA["datlcam1"]
 detector = TrtYoloDetector(
     initial_frame1,
     init_lane_detector(camera_meta),
-    detection_thresh=0.5,
+    detection_thresh=0.4,
 )
 
 date = datetime.datetime.now()
@@ -46,7 +47,7 @@ outvideo_path = curr_folder + "/linear_mapping.avi"
 videowriter = cv2.VideoWriter(
     outvideo_path,
     cv2.VideoWriter_fourcc("M", "J", "P", "G"),
-    30.0,
+    50.0,
     (1920, 540),
 )
 
@@ -67,7 +68,7 @@ while vidcap1.isOpened() and vidcap2.isOpened():
     frame1 = cv2.resize(frame1, dsize=(width1//2, height1//2))
     frame2 = cv2.resize(frame2, dsize=(width2//2, height2//2))
 
-    for l in ["leftlane", "rightlane"]:
+    for l in ["leftlane", "middlelane", "rightlane"]:
         cv2.polylines(frame1, [camera_meta[f"{l}_coords"]], isClosed=True, color=(0, 0, 0), thickness=2)
 
     detection_list, axles = detector.detect(frame1)
@@ -78,11 +79,15 @@ while vidcap1.isOpened() and vidcap2.isOpened():
         cv2.rectangle(frame1, rect[:2], rect[2:], (225,0,0), 2)
         cv2.circle(frame1, btm, 4, (0,0,255), -1)
 
-        btmx_tf = (0.44958882 * btm[0]) + (-3.23493889 * btm[1]) + 1299.903505513684
-        btmy_tf = (0.41243032 * btm[0]) + (-0.07925315 * btm[1]) + 116.09248088227169
-
-        # btmx_tf = (0.39795221 * btm[0]) + (-3.06747396 * btm[1]) + 1269.8043491220583
-        # btmy_tf = (0.38150148 * btm[0]) + (-0.01415993 * btm[1]) + 109.26557888289531
+        if det['lane'] == "1":
+            btmx_tf = int((0.65523379 * btm[0]) + (-3.67679969 * btm[1]) + 1349.9740589597031)
+            btmy_tf = int((0.41925539 * btm[0]) + (-0.04235352 * btm[1]) + 99.19415894167156)
+        elif det['lane'] == "2":
+            btmx_tf = int((0.55305366 * btm[0]) + (-3.3535726 * btm[1]) + 1301.5774568947409)
+            btmy_tf = int((0.37036275  * btm[0]) + (-0.02834603 * btm[1]) + 113.54696288217643)
+        else:
+            btmx_tf = int((0.39657267 * btm[0]) + (-2.85288489 * btm[1]) + 1195.282143258536)
+            btmy_tf = int((0.30479565 * btm[0]) + (0.04620164 * btm[1]) + 108.36117943778677)
 
         cv2.circle(frame2, (int(btmx_tf), int(btmy_tf)), 4, (0,0,255), -1)
 
